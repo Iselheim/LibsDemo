78757eb2899f3510948f196f3d73bf19c98e788b
a.bolka <a.bolka@bosmanager.com>
2020-11-28 12:10:23 +0100

    TryTask

--- src/main/java/pl/bolka/aleksander/vavr/trydemo/task/TryTask.java
+++ src/main/java/pl/bolka/aleksander/vavr/trydemo/task/TryTask.java
@@ -1,5 +1,8 @@
 package pl.bolka.aleksander.vavr.trydemo.task;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -12,8 +15,12 @@ public class TryTask {
     try {
       URL resource = getClass().getClassLoader().getResource(path);
       File file = new File(resource.toURI());
-
+      User user = new ObjectMapper().readValue("content", User.class);
     } catch (URISyntaxException e) {
+      e.printStackTrace();
+    } catch (JsonMappingException e) {
+      e.printStackTrace();
+    } catch (JsonProcessingException e) {
       e.printStackTrace();
     }
 
